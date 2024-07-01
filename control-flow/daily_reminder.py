d995413cd5c803e808aa0293cece0b816a9498ae
sisay27 <sisayanmaw27@gmail.com>
2024-07-01 12:04:54 +0300

    correction is done

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,19 +1,15 @@
-# daily_reminder.py
-
-# Prompt for a Single Task
 task = input("Enter the task description: ")
 priority = input("Enter the priority level (high, medium, low): ")
 time_bound = input("Is the task time-bound? (yes or no): ")
 
-# Process the Task Based on Priority and Time Sensitivity
 if time_bound == "yes":
     match priority:
         case "high":
-            print(f"Reminder: {task} is {priority} Priority task ", end=" ")
+            print(f"Reminder: {task} is {priority} Priority task", end=" ")
         case "medium":
-            print(f"Reminder: {task} is {priority} Priority task ", end=" ")
+            print(f"Reminder: {task} is {priority} Priority task", end=" ")
         case "low":
-            print(f"Reminder: {task} is {priority} Priority task ", end=" ")
+            print(f"Reminder: {task} is {priority} Priority task", end=" ")
 
     if time_bound == "yes":
         print("that requires immediate attention today!")
