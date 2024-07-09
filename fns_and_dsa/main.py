6f47ed8b9187289ad8a6b8bcd47658b466a0de5c
sisay27 <sisayanmaw27@gmail.com>
2024-07-09 11:37:56 +0300

    assignment-2nd

--- fns_and_dsa/main.py
+++ fns_and_dsa/main.py
@@ -1,10 +1,10 @@
 from arithmetic_operations import perform_operation
 
 
-def main ():
+def main():
     print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1=float(input("Enter the first number: "))
+    num2=float(input("Enter the second number: "))
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     result= perform_operation(num1, num2, operation)
