583f0a4e1bcc25c3de2c16997c4459fe7a06152f
sisay27 <sisayanmaw27@gmail.com>
2024-07-16 23:19:27 +0300

    pradiem with correction

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,13 +1,16 @@
 class BankAccount:
     def __init__(self, initial_balance=0):
-        self.account_balance = initial_balance
+        self._account_balance = initial_balance
+
     def deposit(self, amount):
-        self.account_balance += amount
-    def withdraw(self,amount):
-        if self.account_balance >= amount:
-            self.account_balance -= amount
+        self._account_balance += amount
+
+    def withdraw(self, amount):
+        if self._account_balance >= amount:
+            self._account_balance -= amount
             return True
-        else :
+        else:
             return False
+
     def display_balance(self):
-        print(f"Current balance: $".format(self.account_balance))
+        print("Current Balance: ${:.2f}".format(self._account_balance))
