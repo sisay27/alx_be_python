d34605824a0b9d4a982bcd8a004fa8bea4163e04
sisay27 <sisayanmaw27@gmail.com>
2024-07-18 11:59:31 +0300

    robust correction

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,10 +1,10 @@
-def safe_divide(numerator, denominator):
-    try:
-        numerator = float(numerator)
-        denominator = float(denominator)
-        result = numerator/denominator
-        return result
-    except ZeroDivisionError:
-        return "Error: Cannot divide by zero.."
-    except ValueError:
+def safe_divide(numerator, denominator):  
+    try:  
+        numerator = float(numerator)  
+        denominator = float(denominator)  
+        if denominator == 0:  
+            return "Error: Cannot divide by zero."  
+        result = numerator / denominator  
+        return result  
+    except ValueError:  
         return "Error: Please enter numeric values only."
